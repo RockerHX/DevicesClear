bc29d1cb18344dd59879970a449833d62034d1b9
RockerHX <rockerhx@gmail.com>
2018-06-05 20:00:42 +0800

    Initial Commit

---
+++ DevicesClean/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DevicesClean
+//
+//  Created by RockerHX on 2018/6/5.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
